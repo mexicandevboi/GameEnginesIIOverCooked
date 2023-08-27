124ac826dfa71b51166fa4d5a2fa8514dea18a92
nico233 <1601550204@qq.com>
2023-08-27 15:58:08 +0800

    Update player movement script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,15 +7,17 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 7f;
 
+    private bool isWalking;
+
     private void Update() {
         var inputVector = Vector2.zero;
-        if (Input.GetKeyDown(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.W)) {
             inputVector.y = +1;
-        } else if (Input.GetKeyDown(KeyCode.S)) {
+        } else if (Input.GetKey(KeyCode.S)) {
             inputVector.y = -1;
-        } else if (Input.GetKeyDown(KeyCode.A)) {
+        } else if (Input.GetKey(KeyCode.A)) {
             inputVector.x = -1;
-        } else if (Input.GetKeyDown(KeyCode.D)) {
+        } else if (Input.GetKey(KeyCode.D)) {
             inputVector.x = +1;
         }
 
@@ -24,6 +26,13 @@ public class Player : MonoBehaviour
         var moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * (speed * Time.deltaTime);
 
-        transform.forward = moveDir;
+        isWalking = moveDir != Vector3.zero;
+        
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 }
