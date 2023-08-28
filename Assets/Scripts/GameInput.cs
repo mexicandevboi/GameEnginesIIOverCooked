83779f59068feb6fb93ebbf5a0c38356b8a8e3f8
nico233 <1601550204@qq.com>
2023-08-28 18:07:46 +0800

    Use Input System instead of Input Manager

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+
+    private void Awake() {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized() {
+        var inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+        
+        return inputVector;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 7f;
+    [SerializeField] private GameInput gameInput;
 
-    private bool isWalking;
+    [SerializeField] private bool isWalking;
 
     private void Update() {
-        var inputVector = Vector2.zero;
+        /*var inputVector = Vector2.zero;
         if (Input.GetKey(KeyCode.W)) {
             inputVector.y = +1;
         } else if (Input.GetKey(KeyCode.S)) {
@@ -21,7 +22,9 @@ public class Player : MonoBehaviour
             inputVector.x = +1;
         }
 
-        inputVector = inputVector.normalized;
+        inputVector = inputVector.normalized;*/
+
+        var inputVector = gameInput.GetMovementVectorNormalized();
 
         var moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * (speed * Time.deltaTime);
