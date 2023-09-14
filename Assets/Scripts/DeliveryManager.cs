9fec57cfbcabb56bb43de80f77edf3b64668b052
nico233 <1601550204@qq.com>
2023-09-14 16:11:35 +0800

    Add the logic and UI of GameStartCountdown, GamePlayingClock and GameOver

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -19,6 +19,7 @@ public class DeliveryManager : MonoBehaviour
     private float spawnRecipeTimer;
     private float spawnRecipeTimerMax = 4f;
     private int waitingRecipesMax = 4;
+    private int successfulRecipesAmount;
 
     private void Awake() {
         Instance = this;
@@ -71,6 +72,8 @@ public class DeliveryManager : MonoBehaviour
                 if (plateContentsMatchesRecipe) {
                     // Player delivered the correct recipe
 
+                    successfulRecipesAmount++;
+
                     waitingRecipeSOList.RemoveAt(i);
                     
                     OnRecipeCompleted?.Invoke(this ,EventArgs.Empty);
@@ -87,6 +90,10 @@ public class DeliveryManager : MonoBehaviour
 
     public List<RecipeSO> GetWaitingRecipeSOList() {
         return waitingRecipeSOList;
+    }
+
+    public int GetSuccessfulRecipesAmount() {
+        return successfulRecipesAmount;
     }
     
 }
